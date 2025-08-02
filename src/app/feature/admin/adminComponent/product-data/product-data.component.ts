beadbe961a05161cc45a4986883dcb5e69431f33
krishna-bot-cmd <99655hari@gmail.com>
2025-08-02 22:31:01 +0530

    product dto chage

--- src/app/feature/admin/adminComponent/product-data/product-data.component.ts
+++ src/app/feature/admin/adminComponent/product-data/product-data.component.ts
@@ -55,21 +55,20 @@ export class ProductDataComponent implements OnInit {
       updatedItem.append('description', this.productsList[index].description);
       updatedItem.append('price', price);
       updatedItem.append('stockQuantity', this.productsList[index].stockQuantity.toString());
-      updatedItem.append('active', this.productsList[index].active.toString());
-      updatedItem.append('image', this.imageFile);
+      updatedItem.append('image', this.imageFile === undefined ? '' : this.imageFile);
     } else {
       const i = this.productsList.findIndex(p => p.id === productId);
       index = i;
       const editedFlag = this.flagCheck !== {} as any ? JSON.stringify(this.flagCheck) === JSON.stringify(this.productsList[i]) ? false : true : false;
-      alert(this.productsList[i].active)
       if (editedFlag) {
         const price = parseFloat(this.productsList[index].price.toFixed(2)).toString();
         updatedItem.append('name', this.productsList[index].name);
         updatedItem.append('description', this.productsList[index].description);
         updatedItem.append('price', price);
         updatedItem.append('stockQuantity', this.productsList[index].stockQuantity.toString());
-        updatedItem.append('image', this.imageFile);
+        updatedItem.append('image', this.imageFile === undefined ? '' : this.imageFile);
         updatedItem.append('active', this.productsList[index].active.toString());
+        updatedItem.append('discount', this.productsList[index].discount.toString());
       }
     }
     if (Array.from(updatedItem as any).length !== 0) {
